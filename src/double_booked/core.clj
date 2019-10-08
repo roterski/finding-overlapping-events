b90398f8463cea90ed2b1029dddd61f7c2ebd09f
Piotr Roterski <pt.roterski@gmail.com>
2019-10-08 18:44:39 +0200

    Initial commit

---
+++ src/double_booked/core.clj
@@ -0,0 +1,10 @@
+(ns double-booked.core
+  (:gen-class))
+
+(def input-data-1
+  '())
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/double_booked/core_test.clj
@@ -0,0 +1,7 @@
+(ns double-booked.core-test
+  (:require [clojure.test :refer :all]
+            [double-booked.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
